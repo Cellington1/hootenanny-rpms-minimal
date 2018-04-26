355707ddb881dfeb96225b8b2e8db8bb30248b24
Cellington1 <cody.ellington@radiantsolutions.com>
2018-04-26 09:38:44 -0400

    Created new archive script for fortify scan

---
+++ shell/BuildArchiveFortify.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euxo pipefail
+
+## Get variables.
+set +u
+GIT_COMMIT="${1:-geoint-svc}"
+set -u
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/Vars.sh
+
+# The container used to build Hootenanny archive.
+BUILD_IMAGE=${BUILD_IMAGE:-cellington1/rpmbuild-hoot-release}
+
+echo "pea soup"
+
+run_hoot_build_image \
+    -i $BUILD_IMAGE -s rw \
+    /bin/sh -c "su-exec postgres pg_ctl -D /var/lib/pgsql/9.5/data -s start &> /dev/null && /rpmbuild/scripts/hoot-checkout.sh ${GIT_COMMIT} && /rpmbuild/scripts/hoot-archive.sh"
